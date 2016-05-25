c5c5a62abc66a12524e0014c9c1462c6c8ec38c5
f-w <fred.wen@gov.bc.ca>
2016-05-25 13:21:45 -0700

    set uer id from header

--- common/models/transcript-submission.js
+++ common/models/transcript-submission.js
@@ -1,6 +1,6 @@
 module.exports = function(TranscriptSubmission) {
   TranscriptSubmission.beforeRemote('create', function(ctx, unused, next){
-    ctx.req.body.userID = 'aaa'
+    ctx.req.body.userID = ctx.req.get('sm_user') || ctx.req.get('smgov_userdisplayname')
     next()
   })
 };
