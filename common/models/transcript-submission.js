3891d769ea5dba8879e86237e3aa6f3b935b367f
f-w <fred.wen@gov.bc.ca>
2016-06-08 12:00:28 -0700

    force content type to be application/json

--- common/models/transcript-submission.js
+++ common/models/transcript-submission.js
@@ -1,6 +1,8 @@
 module.exports = function(TranscriptSubmission) {
   TranscriptSubmission.beforeRemote('create', function(ctx, unused, next){
-    ctx.req.body.userID = ctx.req.get('sm_user') || ctx.req.get('smgov_userdisplayname')
+    ctx.req.headers['content-type'] = 'application/json'
+    ctx.req.headers['accept'] = 'application/json'
+    ctx.req.body.userID = ctx.req.get('sm_user') || ctx.req.get('smgov_userdisplayname') || 'unknown'
     next()
   })
 };
