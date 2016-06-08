79497d207d68eee599e3a301b32b73fd9dd486ee
f-w <fred.wen@gov.bc.ca>
2016-06-08 15:10:28 -0700

    make sure get request only retrieves current user's transcriptSubmission.

--- common/models/transcript-submission.js
+++ common/models/transcript-submission.js
@@ -1,6 +1,14 @@
-module.exports = function(TranscriptSubmission) {
-  TranscriptSubmission.beforeRemote('create', function(ctx, unused, next){
+module.exports = function (TranscriptSubmission) {
+  TranscriptSubmission.beforeRemote('create', function (ctx, unused, next) {
     ctx.req.body.userID = ctx.req.get('sm_user') || ctx.req.get('smgov_userdisplayname') || 'unknown'
     next()
   })
-};
+
+  TranscriptSubmission.beforeRemote('find', function (ctx, unused, next) {
+    ctx.args.filter = {}
+    ctx.args.filter.where = {
+      "userID": ctx.req.get('sm_user') || ctx.req.get('smgov_userdisplayname') || 'unknown'
+    }
+    next()
+  })
+}
